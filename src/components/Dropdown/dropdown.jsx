77a6b1604be6b9b523d25786bfa4cba588c339d0
Ricky Nguyen <ricky.nguyen@me.com>
2019-01-28 18:32:31 -0600

    Header

--- src/components/Dropdown/dropdown.jsx
+++ src/components/Dropdown/dropdown.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { colors } from '@material-ui/core';
 import "./dropdown.css";
 
 
@@ -29,17 +27,15 @@ function ButtonAppBar(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="transparent">
         <Toolbar >
           <IconButton className={classes.menuButton} color="black" aria-label="Menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" color="black" className={classes.grow}>
-            Keyboards
+            
           </Typography>
           <Button color="black">Login</Button>
         </Toolbar>
-      </AppBar>
     </div>
   );
 }
