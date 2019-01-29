16c3404c0afced2f9efe13d7532b48ec21c494a8
Ricky Nguyen <ricky.nguyen@me.com>
2019-01-28 19:08:45 -0600

    Keyboard layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import Header from './components/Header/header';
 import Dropdown from './components/Dropdown/dropdown';
+import Keyboard from './components/Keyboard/keyboard';
+
 
 
 import './App.css';
@@ -12,7 +14,10 @@ class App extends Component {
       <React.Fragment>
        <Dropdown/>
        <Header/>
-    </React.Fragment>
+       <Keyboard/>
+
+     </React.Fragment>
+     
     );
   }
 }
