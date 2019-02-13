f16510182796b8aa6e85101cf27e8a4b854bc0e6
Ricky Nguyen <ricky.nguyen@me.com>
2019-02-13 12:59:48 -0600

    css

--- src/components/Keyboards/action.js
+++ src/components/Keyboards/action.js
@@ -3,9 +3,11 @@ window.addEventListener('keydown', function (e) {
 	var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
 	var key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
 	if (!audio) return; // stop function if no audio
+	
 	audio.currentTime = 0;
 	audio.play();
 	key.classList.add('playing');
+	
 
 	function removeTransition(e) {
 		if (e.propertyName !== 'transform') return; // Skip if it's not transform
